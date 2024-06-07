fix: gabors are optionally flattened, theoretical tuning curve is corrected in testing_gabor_output, neat: no 3 scaling in BW_image functions + minor clean ups

analysis/analysis_functions.py
@@ -277,9 +277,11 @@ def gabor_tuning(untrained_pars, ori_vec=np.arange(0,180,6)):
     # Reshape the center coordinates into column vectors; repeat and reshape the center coordinates to allow calculating diff_x and diff_y
     x = np.repeat(x_1D, N_pixels, axis=1)
     y = np.repeat(y_1D, N_pixels, axis=0)
-    
+    if len(gabor_filters.shape)==2:
+        gabor_filters = np.reshape(gabor_filters, (untrained_pars.grid_pars.gridsize_Nx **2,untrained_pars.ssn_pars.phases,2,-1)) # the third dimension 2 is for I and E cells, the last dim is the image size
     stimuli = BW_image_full_jit(untrained_pars.BW_image_jax_inp[0:5], x, y, ori_vec, np.zeros(num_ori))
     # Apply Gabor filters to stimuli
+
     gabor_output = numpy.zeros((num_ori, gabor_filters.shape[0],gabor_filters.shape[1],gabor_filters.shape[2]))
     for ori in range(num_ori):
         for grid_ind in range(gabor_filters.shape[0]):
@@ -879,117 +881,6 @@ def filtered_model_response(folder, run_ind, ori_list= np.asarray([55, 125, 0]),
                 plt.savefig(f'{folder}/figures/filt_resp_{run_ind}_{layer}_{sigma_filter}.png')
     return output, SGD_step_inds
 
-"""
-def filtered_model_response_task(folder, run_ind, ori_list= np.asarray([55, 125, 0]), num_noisy_trials = 100, num_SGD_inds = 2, r_noise=False, sigma_filter = 1, gridsize_Nx=9):
-    '''Calculate filtered model response for each orientation in ori_list and for each parameter set (that come from file_name at num_SGD_inds rows)'''
-    file_name = f"{folder}/results_{run_ind}.csv"
-    loaded_orimap = load_orientation_map(folder, run_ind)
-
-    untrained_pars = init_untrained_pars(grid_pars, stimuli_pars, filter_pars, ssn_pars, conv_pars, 
-                    loss_pars, training_pars, pretrain_pars, readout_pars, None, orimap_loaded=loaded_orimap)
-    df = pd.read_csv(file_name)
-    SGD_step_inds = SGD_step_indices(df, num_SGD_inds)
-
-    # Iterate overs SGD_step indices (default is before and after training)
-    for step_ind in SGD_step_inds:
-        # Load parameters from csv for given epoch
-        _, trained_pars_stage2, _ = load_parameters(file_name, iloc_ind = step_ind)
-        J_2x2_m = sep_exponentiate(trained_pars_stage2['log_J_2x2_m'])
-        J_2x2_s = sep_exponentiate(trained_pars_stage2['log_J_2x2_s'])
-        c_E = trained_pars_stage2['c_E']
-        c_I = trained_pars_stage2['c_I']
-        f_E = np.exp(trained_pars_stage2['log_f_E'])
-        f_I = np.exp(trained_pars_stage2['log_f_I'])
-        
-        # Iterate over the orientations
-        for ori in ori_list:
-            # Select orientation from list
-            untrained_pars.stimuli_pars.ref_ori = ori
-
-            # Generate data
-            test_gratings = create_grating_training(untrained_pars.stimuli_pars, num_noisy_trials,untrained_pars.BW_image_jax_inp)
-           
-            label = test_gratings['label']
-            # Create middle and superficial SSN layers *** this is something that would be great to call from outside the SGD loop and only refresh the params that change (and what rely on them such as W)
-            kappa_pre = untrained_pars.ssn_pars.kappa_pre
-            kappa_post = untrained_pars.ssn_pars.kappa_post
-            p_local_s = untrained_pars.ssn_pars.p_local_s
-            s_2x2 = untrained_pars.ssn_pars.s_2x2_s
-            sigma_oris = untrained_pars.ssn_pars.sigma_oris
-            ssn_mid=SSN_mid(ssn_pars=untrained_pars.ssn_pars, grid_pars=untrained_pars.grid_pars, J_2x2=J_2x2_m)
-            ssn_sup=SSN_sup(ssn_pars=untrained_pars.ssn_pars, grid_pars=untrained_pars.grid_pars, J_2x2=J_2x2_s, p_local=p_local_s, oris=untrained_pars.oris, s_2x2=s_2x2, sigma_oris = sigma_oris, ori_dist = untrained_pars.ori_dist, train_ori = ori, kappa_post = kappa_post, kappa_pre = kappa_pre)
-    
-            # Calculate fixed point for data
-            _, _, [_, _], [_, _], [_, _, _, _], [r_mid_ref, r_sup_ref] = vmap_evaluate_model_response(ssn_mid, ssn_sup, test_gratings['ref'], untrained_pars.conv_pars, c_E, c_I, f_E, f_I, untrained_pars.gabor_filters)
-            _, _, [_, _], [_, _], [_, _, _, _], [r_mid_target, r_sup_target] = vmap_evaluate_model_response(ssn_mid, ssn_sup, test_gratings['target'], untrained_pars.conv_pars, c_E, c_I, f_E, f_I, untrained_pars.gabor_filters)
-
-            # Add noise to the responses
-            if r_noise:
-                noise_mid_ref = generate_noise(num_noisy_trials, length = r_mid_ref.shape[1], num_readout_noise = untrained_pars.num_readout_noise)
-                noise_mid_target = generate_noise(num_noisy_trials, length = r_mid_target.shape[1], num_readout_noise = untrained_pars.num_readout_noise)
-                r_mid_ref_noisy = r_mid_ref + noise_mid_ref*np.sqrt(jax.nn.softplus(r_mid_ref))
-                r_mid_target_noisy = r_mid_target + noise_mid_target*np.sqrt(jax.nn.softplus(r_mid_target))
-                noise_sup_ref = generate_noise(num_noisy_trials, length = r_sup_ref.shape[1], num_readout_noise = untrained_pars.num_readout_noise)
-                noise_sup_target = generate_noise(num_noisy_trials, length = r_sup_target.shape[1], num_readout_noise = untrained_pars.num_readout_noise)
-                r_sup_ref_noisy = r_sup_ref + noise_sup_ref*np.sqrt(jax.nn.softplus(r_sup_ref))
-                r_sup_target_noisy = r_sup_target + noise_sup_target*np.sqrt(jax.nn.softplus(r_sup_target))
-            else:
-                r_mid_ref_noisy = r_mid_ref 
-                r_mid_target_noisy = r_mid_target
-                r_sup_ref_noisy = r_sup_ref 
-                r_sup_target_noisy = r_sup_target
-
-            # Smooth responses for each celltype, layer and stimulus type (ref or target) separately with Gaussian filter
-            filtered_r_mid_ref_EI= smooth_data(r_mid_ref_noisy,gridsize_Nx,sigma_filter)  #n_noisy_trials x 648
-            filtered_r_mid_ref_E = vmap_select_type_mid(filtered_r_mid_ref_EI,'E')
-            filtered_r_mid_ref_I = vmap_select_type_mid(filtered_r_mid_ref_EI,'I')
-            filtered_r_mid_ref = np.sum(0.8*filtered_r_mid_ref_E + 0.2 *filtered_r_mid_ref_I, axis=-1) # sum up along phases - should it be sum of squares?
-            filtered_r_mid_target_EI= smooth_data(r_mid_target_noisy,gridsize_Nx,sigma_filter)  #n_noisy_trials x 648
-            filtered_r_mid_target_E = vmap_select_type_mid(filtered_r_mid_target_EI,'E')
-            filtered_r_mid_target_I = vmap_select_type_mid(filtered_r_mid_target_EI,'I')
-            filtered_r_mid_target = np.sum(0.8*filtered_r_mid_target_E + 0.2 *filtered_r_mid_target_I, axis=-1) # order of summing up phases and mixing I-E matters if we change to sum of squares!
-
-            filtered_r_sup_ref_EI = smooth_data(r_sup_ref_noisy,gridsize_Nx,sigma_filter)
-            if filtered_r_sup_ref_EI.ndim == 3:
-                filtered_r_sup_ref_E = filtered_r_sup_ref_EI[:,:,0]
-                filtered_r_sup_ref_I = filtered_r_sup_ref_EI[:,:,1]
-            if filtered_r_sup_ref_EI.ndim == 4:
-                filtered_r_sup_ref_E = filtered_r_sup_ref_EI[:,:,:,0]
-                filtered_r_sup_ref_I = filtered_r_sup_ref_EI[:,:,:,1]
-            filtered_r_sup_ref = 0.8*filtered_r_sup_ref_E + 0.2 *filtered_r_sup_ref_I
-            filtered_r_sup_target_EI = smooth_data(r_sup_target_noisy,gridsize_Nx,sigma_filter)
-            if filtered_r_sup_target_EI.ndim == 3:
-                filtered_r_sup_target_E = filtered_r_sup_target_EI[:,:,0]
-                filtered_r_sup_target_I = filtered_r_sup_target_EI[:,:,1]
-            if filtered_r_sup_target_EI.ndim == 4:
-                filtered_r_sup_target_E = filtered_r_sup_target_EI[:,:,:,0]
-                filtered_r_sup_target_I = filtered_r_sup_target_EI[:,:,:,1]
-            filtered_r_sup_target = 0.8*filtered_r_sup_target_E + 0.2 *filtered_r_sup_target_I
-
-            # Concatenate along orientation responses
-            if ori == ori_list[0] and step_ind==SGD_step_inds[0]:
-                filtered_r_mid_ref_df = filtered_r_mid_ref
-                filtered_r_mid_target_df = filtered_r_mid_target
-                filtered_r_sup_ref_df = filtered_r_sup_ref
-                filtered_r_sup_target_df = filtered_r_sup_target
-
-                ori_df = np.repeat(ori, num_noisy_trials)
-                step_df = np.repeat(step_ind, num_noisy_trials)
-                labels = label
-            else:
-                filtered_r_mid_ref_df = np.concatenate((filtered_r_mid_ref_df, filtered_r_mid_ref))
-                filtered_r_mid_target_df = np.concatenate((filtered_r_mid_target_df, filtered_r_mid_target))
-                filtered_r_sup_ref_df = np.concatenate((filtered_r_sup_ref_df, filtered_r_sup_ref))
-                filtered_r_sup_target_df = np.concatenate((filtered_r_sup_target_df, filtered_r_sup_target))
-                ori_df = np.concatenate((ori_df, np.repeat(ori, num_noisy_trials)))
-                step_df = np.concatenate((step_df, np.repeat(step_ind, num_noisy_trials)))
-                labels = np.concatenate((labels, label))
-
-    # Define output dictionary with keys: ori, SGD_step, r_mid_ref, r_mid_target, r_sup_ref, r_sup_target, labels
-    output = dict(ori = ori_df, SGD_step = step_df, r_mid_ref = filtered_r_mid_ref_df, r_mid_target = filtered_r_mid_target_df, r_sup_ref = filtered_r_sup_ref_df , r_sup_target = filtered_r_sup_target_df , labels = labels)
-
-    return output, SGD_step_inds
-"""
 
 def LMI_Mahal_df(num_training, num_layers, num_SGD_inds, mahal_train_control_mean, mahal_untrain_control_mean, mahal_within_train_mean, mahal_within_untrain_mean, train_SNR_mean, untrain_SNR_mean, LMI_across, LMI_within, LMI_ratio):
     '''

training/model.py
@@ -32,7 +32,7 @@ def evaluate_model_response(
         input_mid = np.reshape(np.matmul(gabor_filters, np.transpose(stimuli)),len(constant_vector))
     else:
         input_mid = np.matmul(gabor_filters, stimuli)
-
+    input_mid = input_mid.ravel() # this might not work with jit
     # Rectify middle layer input before fix point calculation
     SSN_mid_input = np.maximum(0, input_mid) + constant_vector
 

training/util_gabor.py
@@ -188,8 +188,20 @@ def init_untrained_pars(grid_pars, stimuli_pars, filter_pars, ssn_pars, conv_par
                 print('############## After 50 attempts the randomly generated maps did not pass the uniformity test ##############')
                 break
     
-    gabor_filters = create_gabor_filters_ori_map(ssn_ori_map, ssn_pars.phases, filter_pars, grid_pars)
-    
+    gabor_filters = create_gabor_filters_ori_map(ssn_ori_map, ssn_pars.phases, filter_pars, grid_pars, flatten=True)
+    '''
+    # testing the gabor output - should be below 100 - not quite because we do not do phase matching in find_gabor_A?
+    ori=45
+    BW_image_jit_inp_all = BW_image_jax_supp(stimuli_pars, phase = 0)
+    x = BW_image_jit_inp_all[5]
+    y = BW_image_jit_inp_all[6]
+    alpha_channel = BW_image_jit_inp_all[7]
+    mask = BW_image_jit_inp_all[8]
+    background = BW_image_jit_inp_all[9]
+
+    test_stimuli = BW_image_jax(BW_image_jit_inp_all[0:5], x, y, alpha_channel, mask, background, ori, 0)
+    gabor_filters[0,:] @ test_stimuli
+    '''
     oris = ssn_ori_map.ravel()[:, None]
     ori_dist = cosdiff_ring(oris - oris.T, 180)
     
@@ -219,15 +231,7 @@ def BW_image_jax_supp(stimuli_pars, phase=0.0):
     grating_size = round(stimuli_pars.outer_radius * pixel_per_degree)
     gridsize_deg = stimuli_pars.gridsize_deg
     size = int(stimuli_pars.gridsize_deg  * 2 * pixel_per_degree) + 1
-    '''
-    # Generate a 2D grid of coordinates
-    x, y = numpy.mgrid[
-        -grating_size : grating_size + 1.0,
-        -grating_size : grating_size + 1.0,
-    ]
-    x_jax = np.array(x)
-    y_jax = np.array(y)
-    '''
+    
     # following the same pattern as in gabor filter generation
     N_pixels = grating_size*2+1
     x_1D = np.linspace(-gridsize_deg, gridsize_deg, N_pixels, endpoint=True)
@@ -318,7 +322,7 @@ def BW_image_jax(BW_image_const_inp, x, y, alpha_channel, mask, background, ref_
     # Update the background image with result_roi
     combined_image = lax.dynamic_update_slice(background, result_roi, start_indices)
 
-    return combined_image.ravel()
+    return 3*combined_image.ravel()
 
 def BW_image_full(BW_image_const_inp, x, y, ref_ori, jitter):
     '''Do what BW_image_jax does but without the GRAY backgroung'''
@@ -335,7 +339,7 @@ def BW_image_full(BW_image_const_inp, x, y, ref_ori, jitter):
     # Generate the Gabor stimulus
     gabor_sti = _GRAY * (1 + grating_contrast * spatial_component)
     
-    return gabor_sti.ravel()
+    return 3*gabor_sti.ravel()
 
 # Vectorize BW_image function to process batches
 BW_image_vmap = vmap(BW_image_jax, in_axes=(None,None,None,None,None,None,0,0))
@@ -389,8 +393,6 @@ def gabor_filter(x_i, y_i,k,sigma_g,angle,gridsize_deg,degree_per_pixel,phase=0,
     y_1D = np.linspace(-gridsize_deg, gridsize_deg, N_pixels, endpoint=True)
     y_1D = np.reshape(y_1D, (1,N_pixels))
 
-    ########## Construct filter as an attribute ##########
-    
     # Reshape the center coordinates into column vectors; repeat and reshape the center coordinates to allow calculating diff_x and diff_y
     x_2D = np.repeat(x_1D, N_pixels, axis=1)
     diff_x = x_2D - x_i
@@ -432,8 +434,12 @@ def find_gabor_A(
     all_A = []
 
     for ori in indices:
+        
         # create local_stimui_pars to pass it to BW_Gratings
         local_stimuli_pars = StimuliPars()
+        # extract original parameters
+        #grating_size = round(local_stimuli_pars.outer_radius / local_stimuli_pars.degree_per_pixel)
+        #N_pixels = int(grating_size*2+1)
         local_stimuli_pars.gridsize_deg = gridsize_deg
         local_stimuli_pars.k = k
         local_stimuli_pars.outer_radius = gridsize_deg * 2
@@ -443,16 +449,24 @@ def find_gabor_A(
         local_stimuli_pars.jitter_val = 0
         local_stimuli_pars.std = 0
         local_stimuli_pars.ref_ori = ori
-
+        
         # generate test stimuli at ori orientation
         BW_image_jit_inp_all = BW_image_jax_supp(local_stimuli_pars, phase = phase)
         x = BW_image_jit_inp_all[5]
         y = BW_image_jit_inp_all[6]
+        ''' This is how x and y are calculated within BW_image_jax_supp
+        x_1D = np.linspace(-gridsize_deg, gridsize_deg, N_pixels, endpoint=True)
+        x_1D = np.reshape(x_1D, (N_pixels,1))
+        y_1D = np.linspace(-gridsize_deg, gridsize_deg, N_pixels, endpoint=True)
+        y_1D = np.reshape(y_1D, (1,N_pixels))
+        x = np.repeat(x_1D, N_pixels, axis=1)    
+        y = np.repeat(y_1D, N_pixels, axis=0)
+        '''
         alpha_channel = BW_image_jit_inp_all[7]
         mask = BW_image_jit_inp_all[8]
-        background = BW_image_jit_inp_all[9]
+        background = BW_image_jit_inp_all[9] # this is the only 129 x 129, all x, y, alpha_channel, mask are 257x257 and this makes test_stimuli the right size
         
-        test_stimuli = BW_image_jax(BW_image_jit_inp_all[0:5], x, y, alpha_channel, mask, background, ori, 0)
+        test_stimuli = BW_image_jax(BW_image_jit_inp_all[0:5], x, y, alpha_channel, mask, background, ori, 0) #BW_image_full(BW_image_jit_inp_all[0:5], x, y,  ori, 0)#
         
         # Generate Gabor filter at orientation
         gabor = gabor_filter(0, 0,k,sigma_g,ori,gridsize_deg,degree_per_pixel,phase=phase)
@@ -477,7 +491,8 @@ def create_gabor_filters_ori_map(
     ori_map,
     num_phases,
     filter_pars,
-    grid_pars
+    grid_pars, 
+    flatten=False
 ):
     """Create Gabor filters for each orientation and phase in orimap."""
     k=int(num_phases//2)
@@ -513,12 +528,12 @@ def create_gabor_filters_ori_map(
                     filter_pars.conv_factor
                 )                
                 gabors_demean[i+grid_size_1D*j,phases_ind,:] = A * (gabor.ravel()-np.mean(gabor)) # E filters
-    
     gabors_all[:,0:k,0,:] = filter_pars.gE_m*gabors_demean # E filters phase 0 and pi/2
     gabors_all[:,k:2*k,0,:] = - filter_pars.gE_m*gabors_demean # E filters with opposite phases
     gabors_all[:,0:k,1,:] = filter_pars.gI_m*gabors_demean # I filters phase 0 and pi/2
     gabors_all[:,k:2*k,1,:] = - filter_pars.gI_m*gabors_demean # I filters with opposite phases
-
+    if flatten: # flatten the first three dimensions of gabors_all
+        gabors_all = gabors_all.reshape((grid_size_2D*num_phases*2, image_size))
     return np.array(gabors_all)
 
 def create_gabor_filters_ori_map_old(